fc581883bea1716f3aa45afca11a906f9a33e1e1
ShadelessFox <yanuari.channel@gmail.com>
2025-06-29 21:07:30 +0300

    Fix wrong endian in string conversion of GGUUID

--- odradek-game-hfw/src/main/java/sh/adelessfox/odradek/game/hfw/rtti/data/GGUUIDExtension.java
+++ odradek-game-hfw/src/main/java/sh/adelessfox/odradek/game/hfw/rtti/data/GGUUIDExtension.java
@@ -10,16 +10,20 @@ public interface GGUUIDExtension {
         var builder = new StringBuilder(36);
         var format = HexFormat.of();
 
-        format.toHexDigits(builder, object.data0());
-        format.toHexDigits(builder, object.data1());
+        format.toHexDigits(builder, object.data3());
         format.toHexDigits(builder, object.data2());
-        format.toHexDigits(builder, object.data3()).append('-');
+        format.toHexDigits(builder, object.data1());
+        format.toHexDigits(builder, object.data0());
+        builder.append('-');
+        format.toHexDigits(builder, object.data5());
         format.toHexDigits(builder, object.data4());
-        format.toHexDigits(builder, object.data5()).append('-');
+        builder.append('-');
+        format.toHexDigits(builder, object.data7());
         format.toHexDigits(builder, object.data6());
-        format.toHexDigits(builder, object.data7()).append('-');
+        builder.append('-');
         format.toHexDigits(builder, object.data8());
-        format.toHexDigits(builder, object.data9()).append('-');
+        format.toHexDigits(builder, object.data9());
+        builder.append('-');
         format.toHexDigits(builder, object.data10());
         format.toHexDigits(builder, object.data11());
         format.toHexDigits(builder, object.data12());
