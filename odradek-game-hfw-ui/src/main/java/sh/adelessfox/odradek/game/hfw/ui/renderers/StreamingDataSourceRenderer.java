41086f2b9ea210bd45ba74e18070c32a8023a41e
ShadelessFox <yanuari.channel@gmail.com>
2025-10-06 22:48:48 +0200

    Include channel information in `StreamingDataSource`'s renderer

--- odradek-game-hfw-ui/src/main/java/sh/adelessfox/odradek/game/hfw/ui/renderers/StreamingDataSourceRenderer.java
+++ odradek-game-hfw-ui/src/main/java/sh/adelessfox/odradek/game/hfw/ui/renderers/StreamingDataSourceRenderer.java
@@ -1,6 +1,7 @@
 package sh.adelessfox.odradek.game.hfw.ui.renderers;
 
 import sh.adelessfox.odradek.game.hfw.game.ForbiddenWestGame;
+import sh.adelessfox.odradek.game.hfw.rtti.HorizonForbiddenWest.EStreamingDataChannel;
 import sh.adelessfox.odradek.game.hfw.rtti.HorizonForbiddenWest.StreamingDataSource;
 import sh.adelessfox.odradek.rtti.TypeInfo;
 import sh.adelessfox.odradek.ui.Renderer;
@@ -11,9 +12,10 @@ public class StreamingDataSourceRenderer implements Renderer<StreamingDataSource
     @Override
     public Optional<String> text(TypeInfo info, StreamingDataSource object, ForbiddenWestGame game) {
         var graph = game.getStreamingGraph();
-        var text = "%s (%d bytes)".formatted(
+        var text = "%s (%d bytes, %s)".formatted(
             graph.files().get(object.fileId()),
-            object.length()
+            object.length(),
+            EStreamingDataChannel.valueOf(object.channel())
         );
         return Optional.of(text);
     }
