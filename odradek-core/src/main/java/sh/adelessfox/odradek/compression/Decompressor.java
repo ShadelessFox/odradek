af9226bbe3c59b8ec31ecf08ed5699ca5d58ae73
ShadelessFox <yanuari.channel@gmail.com>
2025-12-21 03:38:56 +0100

    Remove `Decompressor#none` and make `decompress` non-checked-throwable

--- odradek-core/src/main/java/sh/adelessfox/odradek/compression/Decompressor.java
+++ odradek-core/src/main/java/sh/adelessfox/odradek/compression/Decompressor.java
@@ -1,26 +1,21 @@
 package sh.adelessfox.odradek.compression;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public abstract sealed class Decompressor
-    permits LZ4Decompressor, NoneDecompressor {
-
-    public static Decompressor none() {
-        return new NoneDecompressor();
-    }
+    permits LZ4Decompressor {
 
     public static Decompressor lz4() {
         return new LZ4Decompressor();
     }
 
-    public abstract void decompress(ByteBuffer src, ByteBuffer dst) throws IOException;
+    public abstract void decompress(ByteBuffer src, ByteBuffer dst);
 
-    public void decompress(byte[] src, int srcLen, byte[] dst, int dstLen) throws IOException {
+    public void decompress(byte[] src, int srcLen, byte[] dst, int dstLen) {
         decompress(src, 0, srcLen, dst, 0, dstLen);
     }
 
-    public void decompress(byte[] src, int srcOff, int srcLen, byte[] dst, int dstOff, int dstLen) throws IOException {
+    public void decompress(byte[] src, int srcOff, int srcLen, byte[] dst, int dstOff, int dstLen) {
         decompress(ByteBuffer.wrap(src, srcOff, srcLen), ByteBuffer.wrap(dst, dstOff, dstLen));
     }
 }
