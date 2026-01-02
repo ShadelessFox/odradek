dd31f1ee60a2739c85fbcabc51ed5fdbe2b3219b
ShadelessFox <yanuari.channel@gmail.com>
2026-01-02 18:16:03 +0100

    Remember last opened object in Open Object dialog

--- odradek-app/src/main/java/sh/adelessfox/odradek/app/ui/menu/main/file/OpenObjectAction.java
+++ odradek-app/src/main/java/sh/adelessfox/odradek/app/ui/menu/main/file/OpenObjectAction.java
@@ -13,18 +13,25 @@ import javax.swing.*;
 @ActionRegistration(text = "&Open Object\u2026", keystroke = "ctrl O")
 @ActionContribution(parent = MainMenu.File.ID)
 public class OpenObjectAction extends Action {
+    private static String lastInput;
+
     @Override
     public void perform(ActionContext context) {
-        String result = JOptionPane.showInputDialog(
+        String result = (String) JOptionPane.showInputDialog(
             JOptionPane.getRootFrame(),
             "Enter [Group ID] : [Object Index]",
             "Open Object",
-            JOptionPane.PLAIN_MESSAGE
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            null,
+            lastInput
         );
 
         if (result == null) {
             return;
         }
+
+        lastInput = result;
 
         int colon = result.indexOf(':');
         int groupId = Integer.parseUnsignedInt(result.substring(0, colon).strip());
