d5ac310d56dec80beea2a10e7406df5670794645
ShadelessFox <yanuari.channel@gmail.com>
2026-01-16 06:55:54 +0100

    Add styled renderer for pointers

--- odradek-ui/src/main/java/sh/adelessfox/odradek/ui/renderers/PointerRenderer.java
+++ odradek-ui/src/main/java/sh/adelessfox/odradek/ui/renderers/PointerRenderer.java
@@ -5,10 +5,20 @@ import sh.adelessfox.odradek.rtti.TypeInfo;
 import sh.adelessfox.odradek.rtti.data.Ref;
 import sh.adelessfox.odradek.rtti.runtime.TypedObject;
 import sh.adelessfox.odradek.ui.Renderer;
+import sh.adelessfox.odradek.ui.components.StyledText;
 
 import java.util.Optional;
 
 public class PointerRenderer implements Renderer.OfObject<Ref<?>, Game> {
+    @Override
+    public Optional<StyledText> styledText(TypeInfo info, Ref<?> object, Game game) {
+        if (object != null && object.get() instanceof TypedObject to) {
+            return Renderer.renderer(to.getType())
+                .flatMap(x -> x.styledText(to.getType(), to, game));
+        }
+        return Optional.empty();
+    }
+
     @Override
     public Optional<String> text(TypeInfo info, Ref<?> object, Game game) {
         if (object == null) {
