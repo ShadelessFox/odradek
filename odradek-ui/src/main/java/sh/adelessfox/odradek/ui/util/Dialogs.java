d8ee8199da0b3a3a7e9c7eb455e8fbc7e1fef9f9
ShadelessFox <yanuari.channel@gmail.com>
2026-04-15 19:40:00 +0200

    Improve wrapping of exception dialog's message

--- odradek-ui/src/main/java/sh/adelessfox/odradek/ui/util/Dialogs.java
+++ odradek-ui/src/main/java/sh/adelessfox/odradek/ui/util/Dialogs.java
@@ -1,6 +1,5 @@
 package sh.adelessfox.odradek.ui.util;
 
-import com.formdev.flatlaf.extras.components.FlatLabel;
 import com.formdev.flatlaf.extras.components.FlatScrollPane;
 import com.formdev.flatlaf.extras.components.FlatTextArea;
 import net.miginfocom.swing.MigLayout;
@@ -17,29 +16,33 @@ public final class Dialogs {
     private Dialogs() {
     }
 
-    public static void showExceptionDialog(Component parentComponent, String title, Throwable throwable) {
+    public static void showExceptionDialog(Component parentComponent, String message, Throwable throwable) {
         var sw = new StringWriter();
         var pw = new PrintWriter(sw);
         throwable.printStackTrace(pw);
 
-        var caption = new FlatLabel();
-        caption.setText(title);
-        caption.setLabelType(FlatLabel.LabelType.large);
+        var caption = new FlatTextArea();
+        caption.setStyle("font: $large.font; background: $Label.background; margin: 0,0,0,0");
+        caption.setText(message);
+        caption.setLineWrap(true);
+        caption.setWrapStyleWord(true);
+        caption.setEditable(false);
+        caption.setCaretPosition(0);
 
         var trace = new FlatTextArea();
         trace.setStyleClass("monospaced");
-        trace.setEditable(false);
         trace.setText(sw.toString().replace("\t", "    "));
+        trace.setEditable(false);
         trace.setCaretPosition(0);
 
-        var pane = new FlatScrollPane();
-        pane.setViewportView(trace);
-        pane.setStyle("border: 1,1,1,1, $Component.borderColor");
+        var tracePane = new FlatScrollPane();
+        tracePane.setViewportView(trace);
+        tracePane.setStyle("border: 1,1,1,1, $Component.borderColor");
 
-        var panel = new JPanel(new MigLayout("ins 0,wrap"));
-        panel.add(caption);
+        var panel = new JPanel(new MigLayout("ins 0,wrap,w 600", "[grow,fill]"));
+        panel.add(new JScrollPane(caption), "h pref::");
         panel.add(new JLabel("Stack trace:"));
-        panel.add(pane, "w :600:,h :250:");
+        panel.add(tracePane, "h :250:");
 
         int result = JOptionPane.showOptionDialog(
             parentComponent,
