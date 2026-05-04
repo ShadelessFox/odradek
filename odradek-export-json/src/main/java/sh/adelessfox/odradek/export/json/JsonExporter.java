df2c8e176002435e71e8ec35ff7f906a3d7fc57c
Julz876 <julio1mat@gmail.com>
2026-05-03 20:04:31 -0400

    Exclude JsonExporter changes from StaticTile fix

--- odradek-export-json/src/main/java/sh/adelessfox/odradek/export/json/JsonExporter.java
+++ odradek-export-json/src/main/java/sh/adelessfox/odradek/export/json/JsonExporter.java
@@ -8,7 +8,6 @@ import sh.adelessfox.odradek.rtti.*;
 import sh.adelessfox.odradek.rtti.data.TypedObject;
 import sh.adelessfox.odradek.rtti.data.Value;
 import sh.adelessfox.odradek.rtti.util.SimpleTypeVisitor;
-import sh.adelessfox.odradek.rtti.util.TypePath;
 
 import java.io.IOException;
 import java.nio.channels.Channels;
@@ -50,36 +49,12 @@ public class JsonExporter implements Exporter.OfSingleOutput<TypedObject> {
     }
 
     private static final class JsonVisitor extends SimpleTypeVisitor<JsonWriter, IOException> {
-        private final TypePath.Builder path = TypePath.builder();
-
         @Override
         public void visitAtom(AtomTypeInfo info, Object object, JsonWriter writer) throws IOException {
             if (object instanceof Number number) {
-                writeNumber(writer, number);
+                writer.value(number);
             } else {
                 writer.value(String.valueOf(object));
-            }
-        }
-
-        private void writeNumber(JsonWriter writer, Number number) throws IOException {
-            try {
-                writer.value(number);
-            } catch (IllegalArgumentException e) {
-                if (e.getMessage() == null || !e.getMessage().startsWith("Numeric values must be finite")) {
-                    throw e;
-                }
-                throw new IllegalArgumentException(
-                    "Invalid non-finite numeric value at " + currentPath() + ": " + number,
-                    e
-                );
-            }
-        }
-
-        private String currentPath() {
-            try {
-                return path.build().toString();
-            } catch (IllegalArgumentException e) {
-                return "o";
             }
         }
 
@@ -98,12 +73,7 @@ public class JsonExporter implements Exporter.OfSingleOutput<TypedObject> {
             JsonWriter writer
         ) throws IOException {
             writer.name(attr.name());
-            path.attr(info, attr);
-            try {
-                super.visitClassAttr(info, attr, object, writer);
-            } finally {
-                path.pop();
-            }
+            super.visitClassAttr(info, attr, object, writer);
         }
 
         @Override
@@ -124,12 +94,7 @@ public class JsonExporter implements Exporter.OfSingleOutput<TypedObject> {
             int index,
             JsonWriter writer
         ) throws IOException {
-            path.index(info, index);
-            try {
-                super.visitContainerItem(info, object, index, writer);
-            } finally {
-                path.pop();
-            }
+            super.visitContainerItem(info, object, index, writer);
         }
 
         @Override
