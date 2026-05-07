73adc12fb10336d8c379f25351c819aee0653728
ShadelessFox <yanuari.channel@gmail.com>
2026-05-07 20:56:08 +0200

    Map unknown fields of DS2 ShaderResource

--- odradek-game-ds2/src/main/java/sh/adelessfox/odradek/game/ds2/rtti/callbacks/ShaderResourceCallback.java
+++ odradek-game-ds2/src/main/java/sh/adelessfox/odradek/game/ds2/rtti/callbacks/ShaderResourceCallback.java
@@ -1,6 +1,7 @@
 package sh.adelessfox.odradek.game.ds2.rtti.callbacks;
 
 import sh.adelessfox.odradek.game.ds2.rtti.DS2;
+import sh.adelessfox.odradek.game.ds2.rtti.DS2TypeReader;
 import sh.adelessfox.odradek.io.BinaryReader;
 import sh.adelessfox.odradek.rtti.data.ExtraBinaryDataCallback;
 import sh.adelessfox.odradek.rtti.factory.TypeFactory;
@@ -10,10 +11,9 @@ import java.io.IOException;
 public final class ShaderResourceCallback implements ExtraBinaryDataCallback<DS2.ShaderResource> {
     @Override
     public void deserialize(BinaryReader reader, TypeFactory factory, DS2.ShaderResource object) throws IOException {
-        var size = reader.readInt();
-        var unk00 = reader.readBytes(32);
-        var unk20 = reader.readByte();
-        var unk21 = reader.readInt();
-        var unk25 = reader.readInt();
+        object.size(reader.readInt());
+        object.unk04(DS2TypeReader.readCompound(DS2.GGUUID.class, reader, factory));
+        object.unk14(DS2TypeReader.readCompound(DS2.GGUUID.class, reader, factory));
+        object.unk24(DS2TypeReader.readCompound(DS2.StreamingDataSource.class, reader, factory));
     }
 }
