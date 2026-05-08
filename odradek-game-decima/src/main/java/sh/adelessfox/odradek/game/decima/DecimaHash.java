798fcf10dc67c5feaf823f56d8bc3053f607dd1b
ShadelessFox <yanuari.channel@gmail.com>
2026-05-08 15:03:22 +0200

    Slightly speed up CRC32 hashing

--- odradek-game-decima/src/main/java/sh/adelessfox/odradek/game/decima/DecimaHash.java
+++ odradek-game-decima/src/main/java/sh/adelessfox/odradek/game/decima/DecimaHash.java
@@ -4,16 +4,17 @@ import wtf.reversed.toolbox.hash.CRCAlgorithm;
 import wtf.reversed.toolbox.hash.HashFunction;
 
 public final class DecimaHash {
-    private static final CRCAlgorithm CRC_ALGORITHM = new CRCAlgorithm(32, 0x1edc6f41, 0, true, true, 0);
+    private static final HashFunction CRC32 = HashFunction.crc(new CRCAlgorithm(32, 0x1edc6f41, 0, true, true, 0));
+    private static final HashFunction MURMUR3 = HashFunction.murmur3(42);
 
     private DecimaHash() {
     }
 
     public static HashFunction murmur3() {
-        return HashFunction.murmur3(42);
+        return MURMUR3;
     }
 
     public static HashFunction crc32() {
-        return HashFunction.crc(CRC_ALGORITHM);
+        return CRC32;
     }
 }
