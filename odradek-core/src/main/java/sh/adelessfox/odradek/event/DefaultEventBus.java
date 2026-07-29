4da04a77d436ae77f81840cc9932b4fe18d9b8f3
ShadelessFox <yanuari.channel@gmail.com>
2026-07-29 05:12:35 +0300

    Add sticky events

--- odradek-core/src/main/java/sh/adelessfox/odradek/event/DefaultEventBus.java
+++ odradek-core/src/main/java/sh/adelessfox/odradek/event/DefaultEventBus.java
@@ -1,19 +1,28 @@
 package sh.adelessfox.odradek.event;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.function.Consumer;
 
 public final class DefaultEventBus implements EventBus {
     private final Map<Class<?>, Set<Consumer<?>>> subscribers = new ConcurrentHashMap<>();
+    private final List<Event.Sticky> stickyEvents = new CopyOnWriteArrayList<>();
 
     @Override
     public <T extends Event> void subscribe(Class<? extends T> topic, Consumer<T> subscriber) {
         subscribers
             .computeIfAbsent(topic, _ -> new CopyOnWriteArraySet<>())
             .add(subscriber);
+
+        for (Event.Sticky event : stickyEvents) {
+            if (topic.isInstance(event)) {
+                publish(event, subscriber);
+            }
+        }
     }
 
     @Override
@@ -28,16 +37,21 @@ public final class DefaultEventBus implements EventBus {
             .forEach(entry -> entry.getValue().remove(subscriber));
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public <T extends Event> void publish(T event) {
+        if (event instanceof Event.Sticky sticky) {
+            stickyEvents.add(sticky);
+        }
+
         subscribers.entrySet().stream()
             .filter(entry -> entry.getKey().isInstance(event))
             .flatMap(entry -> entry.getValue().stream())
-            .forEach(subscriber -> publish(event, (Consumer<T>) subscriber));
+            .forEach(subscriber -> publish(event, subscriber));
     }
 
-    private <T extends Event> void publish(T event, Consumer<? super T> subscriber) {
-        subscriber.accept(event);
+    private <T extends Event> void publish(T event, Consumer<?> subscriber) {
+        @SuppressWarnings("unchecked")
+        var subscriber1 = (Consumer<T>) subscriber;
+        subscriber1.accept(event);
     }
 }
--- odradek-core/src/main/java/sh/adelessfox/odradek/event/Event.java
+++ odradek-core/src/main/java/sh/adelessfox/odradek/event/Event.java
@@ -1,4 +1,15 @@
 package sh.adelessfox.odradek.event;
 
 public interface Event {
+    /**
+     * Marker interface for events that should be sticky.
+     * Sticky events are not removed from the event bus after being published,
+     * and will be delivered to new subscribers immediately upon subscription.
+     *
+     * <h4>Usage notice</h3>
+     * Care must be taken when using sticky events, as they can lead to memory leaks
+     * because they are retained in the event bus basically forever.
+     */
+    interface Sticky extends Event {
+    }
 }
