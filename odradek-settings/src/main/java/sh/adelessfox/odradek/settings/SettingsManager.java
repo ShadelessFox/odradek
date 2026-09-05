f85c992398d8c54f979a847e0dea6fbd39b09748
ShadelessFox <yanuari.channel@gmail.com>
2026-09-06 01:34:32 +0300

    Code style

--- odradek-settings/src/main/java/sh/adelessfox/odradek/settings/SettingsManager.java
+++ odradek-settings/src/main/java/sh/adelessfox/odradek/settings/SettingsManager.java
@@ -142,8 +142,7 @@ public final class SettingsManager implements Settings {
 
         @Override
         public synchronized void set(T value) {
-            Objects.requireNonNull(value, "value");
-            this.value = value;
+            this.value = Objects.requireNonNull(value, "value");
         }
 
         @Override
